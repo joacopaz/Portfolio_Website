f68cf67dee83a1e9f62824cc38cc12e91d275155
joacopaz <joacopaz@gmail.com>
2022-06-03 03:04:57 -0300

    deleted idea

--- Resources/script.js
+++ Resources/script.js
@@ -1,41 +1,9 @@
-const storedWidth = sessionStorage.getItem('containerWidth')
-const storedHeight = sessionStorage.getItem('containerHeight')
 const main = document.querySelector('main')
 const container = document.querySelector('.background-container')
 const background = document.querySelector('.background')
 
-console.log(`Storage is: ${storedWidth} and ${storedHeight}`)
-
 window.onload = async () => {
     document.querySelector('[data-video]').play()
-
-    if (main.offsetWidth < storedWidth) {
-        console.log('Smaller')
-        main.style.minWidth = storedWidth + 'px'
-        await new Promise(r => setTimeout(r, 4));
-        background.style.transition = 'all 5000ms'
-        main.style.transition = 'all 5000ms'
-        container.style.transition = 'all 5000ms'
-        main.style.minWidth = ''
-        await new Promise(r => setTimeout(r, 2000));
-        background.style.transition = ''
-        main.style.transition = ''
-        container.style.transition = ''
-    }
-    if (main.offsetWidth > storedWidth) {
-        console.log('Bigger')
-        main.style.maxWidth = storedWidth + 'px'
-        await new Promise(r => setTimeout(r, 4));
-        background.style.transition = 'all 2000ms'
-        main.style.transition = 'all 2000ms'
-        container.style.transition = 'all 2000ms'
-        main.style.maxWidth = ''
-        await new Promise(r => setTimeout(r, 2000));
-        background.style.transition = ''
-        main.style.transition = ''
-        container.style.transition = ''
-    }
-
 }
 
 const animated = document.querySelectorAll('[data-animated]')
@@ -62,8 +30,6 @@ links.forEach(e => {
         if (href.includes('#')) return false;
         event.preventDefault()
         document.querySelector('.mask').style.opacity = '100%'
-        sessionStorage.setItem('containerHeight', container.offsetHeight)
-        sessionStorage.setItem('containerWidth', container.offsetWidth)
         main.style.opacity = '0%'
         await new Promise(r => setTimeout(r, 500));
         window.location = href
