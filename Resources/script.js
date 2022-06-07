3e45fd62c0e75aa15536046bde8d16ce8b8cb111
joacopaz <joacopaz@gmail.com>
2022-06-07 19:14:22 -0300

    updated responsiveness

--- Resources/script.js
+++ Resources/script.js
@@ -1,45 +1,46 @@
 $(window).on("load", function () {
-    $(".loader").fadeOut(100);
+	$(".loader").fadeOut(100);
 });
 $(window).ready(function () {
-    $(".content").fadeIn(1000);
+	$(".background").fadeIn(1000);
+	if ($("footer")) $("footer").fadeIn(1000);
 });
 
-const main = document.querySelector('main')
-const container = document.querySelector('.background-container')
+const main = document.querySelector("main");
+const container = document.querySelector(".background-container");
 
 window.onload = async () => {
-    document.querySelector('[data-video]').play()
-    document.onload = document.querySelector('.mask').style.opacity = '0%'
-    document.onload = document.querySelector('main').style.opacity = '100%'
-}
+	document.querySelector("[data-video]").play();
+	document.onload = document.querySelector(".mask").style.opacity = "0%";
+	document.onload = document.querySelector("main").style.opacity = "100%";
+};
 
-const animated = document.querySelectorAll('[data-animated]')
-animated.forEach(e => {
-    e.addEventListener('mouseleave', async () => {
-        const lines = e.children[0].children
-        lines[0].style.transitionDelay = '600ms'
-        lines[1].style.transitionDelay = '400ms'
-        lines[2].style.transitionDelay = '200ms'
-        lines[3].style.transitionDelay = '0ms'
-        await new Promise(r => setTimeout(r, 600));
-        for (const line of lines) {
-            line.style.transitionDelay = ''
-        }
-    })
-})
+const animated = document.querySelectorAll("[data-animated]");
+animated.forEach((e) => {
+	e.addEventListener("mouseleave", async () => {
+		const lines = e.children[0].children;
+		lines[0].style.transitionDelay = "600ms";
+		lines[1].style.transitionDelay = "400ms";
+		lines[2].style.transitionDelay = "200ms";
+		lines[3].style.transitionDelay = "0ms";
+		await new Promise((r) => setTimeout(r, 600));
+		for (const line of lines) {
+			line.style.transitionDelay = "";
+		}
+	});
+});
 
-const links = document.querySelectorAll('a')
-links.forEach(e => {
-    e.addEventListener('click', async (event) => {
-        const href = event.target.closest('a').href
-        if (href.includes('#')) return false;
-        if (href.includes('_blank')) {
-            window.open(href, '_blank');
-        };
-        event.preventDefault()
-        document.querySelector('.mask').style.opacity = '100%'
-        main.style.opacity = '0%'
-        window.location = href
-    })
-})
+const links = document.querySelectorAll("a");
+links.forEach((e) => {
+	e.addEventListener("click", async (event) => {
+		const href = event.target.closest("a").href;
+		if (href.includes("#")) return false;
+		if (href.includes("_blank")) {
+			window.open(href, "_blank");
+		}
+		event.preventDefault();
+		document.querySelector(".mask").style.opacity = "100%";
+		main.style.opacity = "0%";
+		window.location = href;
+	});
+});
