6a1dacbf0f4a5f55dbd7500a99b47b10175017f1
joacopaz <joacopaz@gmail.com>
2022-06-08 23:23:47 -0300

    finished road map

--- Resources/script-skills.js
+++ Resources/script-skills.js
@@ -6,6 +6,8 @@ $(window).ready(() => {
 	}
 	document.querySelector(".expand-button-skill").childNodes[0].click();
 	document.querySelector(".expand-button-skill").childNodes[0].click();
+	document.querySelector(".expand-button-roadmap").childNodes[0].click()
+	document.querySelector(".expand-button-roadmap").childNodes[0].click()
 	document
 		.querySelector(".expand-button-skill")
 		.addEventListener("click", () => {
@@ -63,6 +65,52 @@ expandSkill.addEventListener("keyup", (evt) => {
 	}
 });
 
+const expandRoadmap = document.querySelector(".expand-button-roadmap");
+
+expandRoadmap.addEventListener("mouseenter", () => {
+	expandRoadmap.classList.add("fa-beat-fade");
+});
+expandRoadmap.addEventListener("mouseleave", () => {
+	expandRoadmap.classList.remove("fa-beat-fade");
+});
+expandRoadmap.addEventListener("focusin", () => {
+	expandRoadmap.classList.add("fa-beat-fade");
+});
+expandRoadmap.addEventListener("focusout", () => {
+	expandRoadmap.classList.remove("fa-beat-fade");
+});
+expandRoadmap.addEventListener("click", (evt) => {
+	if (evt.target.parentNode.dataset.expanded === "false") {
+		document.querySelector(".roadmap-implementation").style.display = "";
+		document.querySelector(".roadmap > aside").style.display = "";
+		document.querySelector(".roadmap > p").style.display = "";
+		// document.querySelector(".roadmap > h2").style.display = "";
+		evt.target.classList.remove("fa-square-plus");
+		evt.target.classList.add("fa-circle-minus");
+		evt.target.parentNode.dataset.expanded = "true";
+	} else {
+		document.querySelector(".roadmap-implementation").style.display = "none";
+		document.querySelector(".roadmap > aside").style.display = "none";
+		document.querySelector(".roadmap > p").style.display = "none";
+		evt.target.classList.remove("fa-circle-minus");
+		evt.target.classList.add("fa-square-plus");
+		evt.target.parentNode.dataset.expanded = "false";
+		const bars = document.querySelectorAll('[data-set="true"]');
+		bars.forEach((bar) => {
+			bar.ldBar.set(0);
+			bar.dataset.set = "false";
+		});
+	}
+});
+expandRoadmap.addEventListener("keyup", (evt) => {
+	evt.preventDefault();
+	if (evt.keyCode === 32 || evt.keyCode === 13) {
+		const icon = evt.target.childNodes[0];
+		icon.click();
+	}
+});
+
+
 document.querySelectorAll(".expand-button").forEach((button) => {
 	button.addEventListener("mouseenter", () => {
 		button.classList.add("fa-beat-fade");
@@ -102,27 +150,37 @@ document.querySelectorAll(".expand-button").forEach((button) => {
 
 document.querySelectorAll(".destination i").forEach((e) => {
 	e.addEventListener("mouseenter", () => {
-		// if (!e.className.includes("fa-beat")) e.classList.add("fa-beat");
+		e.classList.add('fa-beat')
+		e.parentElement.style.color = 'rgb(157, 0, 255)'
 	});
 	e.addEventListener("mouseleave", () => {
-		// if (e.className.includes("fa-beat")) e.classList.remove("fa-beat");
+		if (e.dataset.active !== 'true') {
+			e.classList.remove('fa-beat')
+			e.parentElement.style.color = 'red'
+		}
 	});
 	e.addEventListener("click", () => {
-		document.querySelectorAll("i").forEach(i => {
-			if (i.className.includes("beat")) i.classList.remove("fa-beat")
-		})
-		if (!e.className.includes("beat")) {
-			e.classList.add("fa-beat")
-		} else {
-			e.classList.remove("fa-beat")
-		}
+		document.querySelectorAll(".tooltip").forEach(e => e.style.opacity = "")
 		const tooltip = e.closest(".roadmap-element").querySelector(".tooltip");
-		if (tooltip.style.opacity === "") {
-			document.querySelectorAll(".tooltip").forEach(e => e.style.opacity = "")
-			tooltip.style.opacity = "100"
-		} else {
+		if (e.dataset.active && e.dataset.active === 'true') {
 			tooltip.style.opacity = ""
+			e.classList.remove("fa-beat")
+			e.dataset.active = 'false'
+		} else {
+			document.querySelectorAll('.destination i').forEach(i => {
+				i.dataset.active = 'false'
+			})
+			tooltip.style.opacity = "100"
+			e.dataset.active = 'true'
+			e.classList.add("fa-beat")
 		}
-
+		document.querySelectorAll(".destination i").forEach(i => {
+			if (i.className.includes("beat") && i.dataset.active === 'false') {
+				i.classList.remove("fa-beat")
+			}
+			if (i.dataset.active === 'false') {
+				i.parentElement.style.color = 'red'
+			}
+		})
 	});
 });
