ee335f9b78250e4ded5e590d6bc03534b017e620
joacopaz <joacopaz@gmail.com>
2022-06-10 19:29:24 -0300

    added resume

--- Resources/script.js
+++ Resources/script.js
@@ -35,7 +35,7 @@ animated.forEach((e) => {
 
 const links = document.querySelectorAll("a");
 links.forEach((e) => {
-	if (e.href = "./Resources/Joaquin-Paz-Resume.pdf") return
+	if (e.href === "./Resources/Joaquin-Paz-Resume.pdf") return
 	e.addEventListener("click", async (event) => {
 		const href = event.target.closest("a").href;
 		if (href.includes("#")) return false;
