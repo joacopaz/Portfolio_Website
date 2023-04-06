75ebe183df7e16ae75bcf94dbc9b8004435c478d
joacopaz <joacopaz@gmail.com>
2023-04-06 06:51:39 -0300

    Updating projects and optimizing images

--- Resources/script-projects.js
+++ Resources/script-projects.js
@@ -1,16 +1,20 @@
-const projectLinks = document.querySelectorAll('[data-project-image]')
+const projectLinks = document.querySelectorAll("[data-project-image]");
 
-projectLinks.forEach(link => {
-    link.addEventListener('click', (evt) => {
-        window.open(evt.target.dataset.projectImage, '_blank');
-    })
-})
+projectLinks.forEach((link) => {
+	link.addEventListener("click", (evt) => {
+		window.open(evt.target.dataset.projectImage, "_blank");
+	});
+});
 
-document.addEventListener('swiped-right', () => {
-    document.querySelector('[data-carrousel-button = "prev"]').click()
-    console.log('swiped-right')
-})
-document.addEventListener('swiped-left', () => {
-    document.querySelector('[data-carrousel-button = "next"]').click()
-    console.log('swiped-left')
-})
+const videos = document.querySelectorAll(".slide video");
+videos.forEach((video) => (video.onclick = (evt) => console.log("clicked")));
+
+document.addEventListener("swiped-right", () => {
+	document.querySelector('[data-carrousel-button = "prev"]').click();
+	console.log("swiped-right");
+});
+document.addEventListener("swiped-left", () => {
+	document.querySelector('[data-carrousel-button = "next"]').click();
+	console.log("swiped-left");
+});
+
